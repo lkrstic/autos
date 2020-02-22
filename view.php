471942786812c66a1f275523daf6aec11e14e83f
lkrstic <lanakrstic@gmail.com>
2020-02-22 12:16:01 -0500

    added pdo object and query to view.php

--- view.php
+++ view.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once "pdo.php";
 ?>
 <html>
 <head>
@@ -8,10 +9,23 @@ session_start();
 <body>
   <?php
   if (!isset($_SESSION['user'])) {
-    echo '<p>Please <a href="login.php">log in</a>.</p>';
+    echo '<p>Please <a href="login.php">log in</a> first.</p>';
   } else { ?>
     <h1>Welcome!</h1>
     <p><a href="add.php">Add New</a> | <a href="logout.php">Log out</a></p>
-  <?php } ?>
+    <h2>Saved Automobiles</h2>
+    <?php
+    $stmt=$pdo->query("SELECT * FROM autos");
+    $rows=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($rows == false) {
+      echo "<p>No automobiles saved yet. Try adding some.</p>";
+    } else {
+      echo "<ul>";
+      foreach ($rows as $row) {
+        echo "<li>".$row['year']." ".$row['make']." ".$row['model']." - ".$row['mileage']." miles</li>";
+      }
+      echo "</ul>";
+    }
+  } ?>
 </body>
 </html>
