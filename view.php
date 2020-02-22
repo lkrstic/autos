a1bb1bf2b697e6416d028734a9d5ec1dfe17cd61
lkrstic <lanakrstic@gmail.com>
2020-02-22 17:25:34 -0500

    Move check for logged in to model in view.php

--- view.php
+++ view.php
@@ -1,31 +1,29 @@
 <?php
 session_start();
 require_once "pdo.php";
+if (!isset($_SESSION['user'])) {
+  die('You must be logged in to view this page.');
+}
 ?>
 <html>
 <head>
   <title>Automobile Tracker</title>
 </head>
 <body>
+  <h1>Welcome, <?php echo $_SESSION['user']; ?>!</h1>
+  <p><a href="add.php">Add New</a> | <a href="logout.php">Log out</a></p>
+  <h2>Saved Automobiles</h2>
   <?php
-  if (!isset($_SESSION['user'])) {
-    echo '<p>Please <a href="login.php">log in</a> first.</p>';
-  } else { ?>
-    <h1>Welcome, <?php echo $_SESSION['user']; ?>!</h1>
-    <p><a href="add.php">Add New</a> | <a href="logout.php">Log out</a></p>
-    <h2>Saved Automobiles</h2>
-    <?php
-    $stmt=$pdo->query("SELECT * FROM autos");
-    $rows=$stmt->fetchAll(PDO::FETCH_ASSOC);
-    if ($rows == false) {
-      echo "<p>No automobiles saved yet. Try adding some.</p>";
-    } else {
-      echo "<ul>";
-      foreach ($rows as $row) {
-        echo "<li>".$row['year']." ".$row['make']." ".$row['model']." - ".$row['mileage']." miles</li>";
-      }
-      echo "</ul>";
+  $stmt=$pdo->query("SELECT * FROM autos");
+  $rows=$stmt->fetchAll(PDO::FETCH_ASSOC);
+  if ($rows == false) {
+    echo "<p>No automobiles saved yet. Try adding some.</p>";
+  } else {
+    echo "<ul>";
+    foreach ($rows as $row) {
+      echo "<li>".$row['year']." ".$row['make']." ".$row['model']." - ".$row['mileage']." miles</li>";
     }
+    echo "</ul>";
   } ?>
 </body>
 </html>
