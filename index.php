b9abbc450c96bedc0e893061c252e2ddef5a163c
lkrstic <lanakrstic@gmail.com>
2020-02-28 19:15:07 -0500

    Begin refactoring index.php for removal of view.php

--- index.php
+++ index.php
@@ -1,12 +1,43 @@
-<!DOCTYPE html>
+<?php
+session_start();
+require_once "pdo.php";
+?>
 <html>
 <head>
-  <title>Auto Database Welcome</title>
+  <title>Automobile Tracker</title>
 </head>
 <body>
-  <h1>Automobile Database Login</h1>
-  <p>Please <a href="login.php">Login</a></p>
-  <p>Go to <a href="view.php">view.php</a> without logging in - this should fail.</p>
-  <p>Go to <a href="add.php">add.php</a> without logging in - this should fail.</p>
+<?php
+if (!isset($_SESSION['user'])) {
+  echo '<h1>Welcome!</h1>';
+  echo '<p>Please <a href="login.php">Login</a></p>';
+  echo '<p>Go to <a href="add.php">add.php</a> without logging in - this should fail.</p>';
+} else {
+  echo '<h1>Welcome, '.$_SESSION['user'].'</h1>';
+  echo '<h2>Saved Automobiles</h2>';
+  $stmt=$pdo->query("SELECT * FROM autos");
+  $rows=$stmt->fetchAll(PDO::FETCH_ASSOC);
+  if($rows !== false) {
+    echo '<table border="1">';
+      echo "<tr>";
+        echo "<td>Make</td>";
+        echo "<td>Model</td>";
+        echo "<td>Year</td>";
+        echo "<td>Mileage</td>";
+        echo "<td>Action</td>";
+      echo "</tr>";
+      foreach($rows as $row) {
+        echo "<tr>";
+          echo "<td>".$row['make']."</td>";
+          echo "<td>".$row['model']."</td>";
+          echo "<td>".$row['year']."</td>";
+          echo "<td>".$row['mileage']."</td>";
+          //echo "<td>".."</td>";
+        echo "</tr>";
+      }
+    echo "</table>";
+  }
+}
+?>
 </body>
 </html>
